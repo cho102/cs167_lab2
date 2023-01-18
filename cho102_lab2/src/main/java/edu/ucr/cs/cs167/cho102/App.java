327b9a15ab458172af2145b8f91ff707c4f82128
Cindy Ho <cho102@ucr.edu>
2023-01-18 00:21:12 +0000

    at home in progress

--- cho102_lab2/src/main/java/edu/ucr/cs/cs167/cho102/App.java
+++ cho102_lab2/src/main/java/edu/ucr/cs/cs167/cho102/App.java
@@ -1,5 +1,7 @@
 package edu.ucr.cs.cs167.cho102;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        if (args.length !=2) {
+            System.err.println("Incorrect number of arguments! Expected two arguments.");
+            System.exit(-1);
+        }
+        else {
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+            Path inputArg = new Path(args[0]);
+            Path outputArg = new Path(args[1]);
+        }
     }
 }
